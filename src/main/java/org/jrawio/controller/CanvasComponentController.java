c0bdf3706496975a14ca3d652a71732047b33243
zihanghong <hongzihang979@gmail.com>
2025-06-01 23:26:25 +0800

    feat: 完成初始布局
    居中画布

--- src/main/java/org/jrawio/controller/CanvasComponentController.java
+++ src/main/java/org/jrawio/controller/CanvasComponentController.java
@@ -3,6 +3,8 @@ package org.jrawio.controller;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
 public class CanvasComponentController {
@@ -10,7 +12,28 @@ public class CanvasComponentController {
     private Canvas gridCanvas;
 
     @FXML
+    private Pane canvasPane;
+
+    @FXML
     public void initialize() {
+        // 设置canvasPane的大小
+        double paneWidth = gridCanvas.getWidth() + 500;
+        double paneHeight = gridCanvas.getHeight() + 500;
+        canvasPane.setPrefWidth(paneWidth);
+        canvasPane.setPrefHeight(paneHeight);
+
+        // 让gridCanvas居中
+        gridCanvas.setLayoutX((paneWidth - gridCanvas.getWidth()) / 2);
+        gridCanvas.setLayoutY((paneHeight - gridCanvas.getHeight()) / 2);
+
+        // 添加阴影效果
+        DropShadow dropShadow = new DropShadow();
+        dropShadow.setRadius(20);
+        dropShadow.setOffsetX(5);
+        dropShadow.setOffsetY(5);
+        dropShadow.setColor(Color.rgb(50, 50, 50, 0.4));
+        gridCanvas.setEffect(dropShadow);
+
         drawGrid();
     }
 
@@ -20,6 +43,11 @@ public class CanvasComponentController {
         double gridSize = 20; // 网格间隔
 
         GraphicsContext gc = gridCanvas.getGraphicsContext2D();
+
+        // 填充白色背景
+        gc.setFill(Color.WHITE);
+        gc.fillRect(0, 0, width, height);
+
         gc.setStroke(Color.LIGHTGRAY);
         gc.setLineWidth(1);
 
---
+++ src/main/java/org/jrawio/controller/CenterPanelController.java
@@ -0,0 +1,18 @@
+package org.jrawio.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+
+public class CenterPanelController {
+    @FXML
+    private ScrollPane centerScrollPane;
+
+    @FXML
+    public void initialize() {
+        // 等待布局完成后设置滚动条位置
+        centerScrollPane.viewportBoundsProperty().addListener((obs, oldVal, newVal) -> {
+            centerScrollPane.setHvalue(0.5);
+            centerScrollPane.setVvalue(0.5);
+        });
+    }
+}
