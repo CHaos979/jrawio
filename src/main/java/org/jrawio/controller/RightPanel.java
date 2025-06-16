4963245f03bf844bf3fce22e90be115880737c2f
zihanghong <hongzihang979@gmail.com>
2025-06-16 23:35:34 +0800

    add: 添加右面板控制器

---
+++ src/main/java/org/jrawio/controller/RightPanel.java
@@ -0,0 +1,11 @@
+package org.jrawio.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
+public class RightPanel {
+    @FXML
+    private VBox rightPanelRoot;
+
+    // 在这里添加右侧面板的逻辑和事件处理方法
+}
