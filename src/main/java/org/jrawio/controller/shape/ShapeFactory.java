69ef2594304ddd294d60e1ad97a1e81ed28b3423
zihanghong <hongzihang979@gmail.com>
2025-06-21 22:05:16 +0800

    feat: 规范注释

--- src/main/java/org/jrawio/controller/shape/ShapeFactory.java
+++ src/main/java/org/jrawio/controller/shape/ShapeFactory.java
@@ -1,12 +1,20 @@
 package org.jrawio.controller.shape;
 
 /**
- * Shape工厂类
+ * 形状工厂类
+ * <p>
+ * 负责创建不同类型的形状实例，支持通过枚举类型或字符串标识符创建形状。
  */
 public class ShapeFactory {
     
     /**
-     * 根据形状类型创建对应的Shape实例
+     * 根据形状类型创建对应的形状实例
+     * 
+     * @param shapeType 形状类型枚举
+     * @param width 形状宽度
+     * @param height 形状高度
+     * @return 创建的形状实例
+     * @throws IllegalArgumentException 当形状类型不支持时抛出
      */
     public static Shape createShape(ShapeType shapeType, double width, double height) {
         switch (shapeType) {
@@ -20,7 +28,13 @@ public class ShapeFactory {
     }
     
     /**
-     * 根据字符串标识符创建对应的Shape实例
+     * 根据字符串标识符创建对应的形状实例
+     * 
+     * @param identifier 形状标识符字符串
+     * @param width 形状宽度
+     * @param height 形状高度
+     * @return 创建的形状实例
+     * @throws IllegalArgumentException 当形状标识符不支持时抛出
      */
     public static Shape createShape(String identifier, double width, double height) {
         for (ShapeType type : ShapeType.values()) {
