6d6a573883cb4e718b220d4259f3a27243d0621b
zihanghong <hongzihang979@gmail.com>
2025-06-21 19:29:27 +0800

    feat: 修复lombok依赖

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,8 @@ module jrawio {
     requires javafx.controls;
     requires javafx.fxml;
     requires transitive javafx.graphics;
-    requires lombok;
-    
+    requires static lombok;
+
     opens org.jrawio to javafx.fxml;
     exports org.jrawio;
     opens org.jrawio.controller to javafx.fxml;
